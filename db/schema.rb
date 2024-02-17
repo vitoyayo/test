d2fa66a98673869a16530261694d998716787058
Victor Cedeño <vicedeno@gmail.com>
2024-02-17 13:22:26 -0300

    update column on users

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_02_181426) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_17_155140) do
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.string "age"
+    t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
